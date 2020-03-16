ec430cb5b3c970d14e6530ea360273876b1613e5
lishunan246 <lishunan246@gmail.com>
2020-03-16 23:17:29 +0800

    fix

--- tests/test_post.py
+++ tests/test_post.py
@@ -6,8 +6,17 @@ import os
 import requests
 
 
-def test_answer():
+def test_update():
     with open(os.path.join("tests", "update.xml"), encoding='utf-8') as f:
+        lines = "".join(f.readlines())
+
+        logging.info(lines)
+    r = requests.post(f'http://127.0.0.1:5000/WebSub/{os.environ.get("WEBSUB_TOKEN")}', data=lines.encode("utf-8"))
+    logging.info(r)
+
+
+def test_delete():
+    with open(os.path.join("tests", "delete.xml"), encoding='utf-8') as f:
         lines = "".join(f.readlines())
 
         logging.info(lines)
