78927792e4d7173177d5a7250b4472439a5f173d
lishunan246 <lishunan246@gmail.com>
2020-03-16 21:02:19 +0800

    fix

--- KaguraMeaLive/schema.py
+++ KaguraMeaLive/schema.py
@@ -13,8 +13,34 @@ class Channel(db.Model):
     id = db.Column(db.String(50, collation=default_collation), primary_key=True)
     name = db.Column(db.String(50, collation=default_collation), nullable=False, index=True)
     subscribe = db.Column(db.Boolean, nullable=False, default=True)
-    last_subscribe = db.Column(db.DateTime(timezone=True))
+    last_subscribe = db.Column(db.TIMESTAMP(timezone=True))
     topic_url = db.Column(db.String(150, collation=default_collation), nullable=False, index=True)
+
+
+class Video(db.Model):
+    __table_args__ = {'mysql_charset': 'utf8mb4', 'mysql_collate': default_collation}
+
+    id = db.Column(db.String(50, collation=default_collation), primary_key=True)
+    channel_id = db.Column(db.String(50, collation=default_collation), index=True, nullable=False)
+    title = db.Column(db.String(100, collation=default_collation), index=True)
+    title_zh = db.Column(db.String(100, collation=default_collation))
+    publish_time = db.Column(db.TIMESTAMP(timezone=True))
+    last_update = db.Column(db.TIMESTAMP(timezone=True))
+    deleted = db.Column(db.Boolean, nullable=False, default=False)
+
+    is_livestream = db.Column(db.Boolean, nullable=True, default=False)
+    actual_start_time = db.Column(db.TIMESTAMP(timezone=True))
+    scheduled_start_time = db.Column(db.TIMESTAMP(timezone=True))
+    concurrent_viewers = db.Column(db.Integer)
+    active_livechat_id = db.Column(db.String(50, collation=default_collation), index=True)
+
+
+class Notification(db.Model):
+    __table_args__ = {'mysql_charset': 'utf8mb4', 'mysql_collate': default_collation}
+
+    id = db.Column(db.Integer, primary_key=True)  # Auto-increment should be default
+    content = db.Column(db.Text(collation=default_collation), nullable=False)
+    last_update = db.Column(db.TIMESTAMP(timezone=True))
 
 
 def init_db():
