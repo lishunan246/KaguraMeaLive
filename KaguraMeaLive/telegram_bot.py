fd70d598b0dde811a08f69c5b8062b24b90a71e6
lishunan246 <lishunan246@gmail.com>
2020-03-26 23:28:07 +0800

    fix

--- KaguraMeaLive/telegram_bot.py
+++ KaguraMeaLive/telegram_bot.py
@@ -17,9 +17,12 @@ class TelegramBot:
 
         for chat in channel.chats:
             try:
+                app.logger.info(f"bot start handle chat: {chat.id} for {n.video_id}")
+
                 message = db.session.query(Message).filter_by(chat_id=chat.id, video_id=n.video_id).one_or_none()
                 if message:
                     if message.text != text:
+                        app.logger.info(f"Message text is not same.")
                         self.bot.editMessageText(text, chat.id, message.id, disable_web_page_preview=True, timeout=1)
                         message.text = text
                         db.session.merge(message)
@@ -27,9 +30,12 @@ class TelegramBot:
                     else:
                         app.logger.info("Message text is same.")
                 else:
+                    app.logger.info("Message text needs create.")
                     m = self.bot.send_message(chat.id, text, disable_web_page_preview=True, timeout=1)
                     message = Message(text=text, message_id=m.message_id, chat_id=chat.id, video_id=n.video_id)
                     db.session.merge(message)
                     db.session.commit()
+                app.logger.info(f"bot handled chat: {chat.id} for {n.video_id}")
+
             except Exception as e:
                 app.logger.error(f'when alert: {e}')
